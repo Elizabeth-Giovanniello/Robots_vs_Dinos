587ee463104fa0214cfef744d085c6b15314b70a
Lizzy <Lizzy@Lizzys-MacBook-Air.local>
2022-01-03 17:09:18 -0500

    Added to logic of dino_turn, robo_turn, and battle methods within Battlefield class.

--- battlefield.py
+++ battlefield.py
@@ -12,20 +12,34 @@ class Battlefield:
         self.display_welcome()
         self.show_dino_opponent_options()
         self.show_robo_opponent_options()
-        self.coin_flip()
         self.battle()
 
     def display_welcome(self):
         print("Welcome to the ultimate battle of the ages, where we pit the ingenuity of modern technology against the brute strength of the prehistoric era! In this no holds barred fight to the death, I give you: Robots versus Dinosaurs!")
     
     def battle(self):
-        pass
+        self.current_team = self.coin_flip()
+        while len(self.herd.dinosaurs) > 0 and len(self.fleet.robots) >0:
+            if self.current_team == "Team Robot":
+                self.robo_turn()
+                self.current_team = "Team Dinosaur"
+            else: 
+                self.dino_turn()
+                self.current_team = "Team Robot"
 
-    def dino_turn(self, dinosaur):
-        pass
 
-    def robo_turn(self, robot):
-        pass
+    def dino_turn(self):
+        self.attacker = random.choice(self.herd.dinosaurs)
+        self.target = random.choice(self.fleet.robots)
+        print(f"This round we have {self.attacker} attacking {self.target}. ")
+        self.attacker.attack(self.target)
+
+    def robo_turn(self):
+        self.attacker = random.choice(self.fleet.robots)
+        self.target = random.choice(self.herd.dinosaurs)
+        print(f"This round we have {self.attacker} attacking {self.target}. ")
+        self.attacker.attack(self.target)
+
 
     def show_dino_opponent_options(self):
         self.herd.create_herd()
@@ -40,8 +54,7 @@ class Battlefield:
             print("Winner: Robots") #COME BACK TO THIS
 
     def coin_flip(self):
-        competitors = ["Team Robot", "Team Dinosaur"]
-        starting_team = random.choice(competitors)
-        print(f"A random coin flip has determined that {starting_team} will have the first turn. ")
-        if starting_team == "Team Robot":
-            self.robo_turn()
+        self.competitors = ["Team Robot", "Team Dinosaur"]
+        self.starting_team = random.choice(self.competitors)
+        print(f"A random coin flip has determined that {self.starting_team} will have the first turn. ")
+        return self.starting_team
