2995f1cc6c5862dc55c6ca0923eddf45f96f1000
Lizzy <Lizzy@Lizzys-MacBook-Air.local>
2022-01-03 12:20:46 -0500

    Created methods and attributes within herd, fleet, and battlefield classes following the UML.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-#Using the concepts of OOP by creating classes and using objects (instances of those classes) to interact with each other, create a console application that will have robots and dinosaurs fight in a battle.
+#Using the concepts of OOP by creating classes and using objects (instances of those classes) to interact with each other, 
+# create a console application that will have robots and dinosaurs fight in a battle.
 
-#Before you begin the last user story that is highlighted in grey, write an algorithm that represents the steps of the robots in the fleet and the dinosaurs in the herd battling. Think about the steps that need to happen to implement the functionality. Please submit to your instructor Slack channel once completed for approval to start coding.
+#Before you begin the last user story that is highlighted in grey, write an algorithm that represents the steps of the 
+# robots in the fleet and the dinosaurs in the herd battling. Think about the steps that need to happen to implement the functionality. 
+# Please submit to your instructor Slack channel once completed for approval to start coding.
 
 #User stories:
 
@@ -14,13 +17,15 @@
 
 #(/10 points): As a developer, I want to instantiate three Robot objects and three Dinosaur objects and assign the appropriate values to all the objects.
 
-#(/10 points): As a developer, I want the created Robot objects to be stored in a Fleet and the created Dinosaur objects to be stored in a Herd (the Fleet and Herd must use a List to store the objects).
+#(/10 points): As a developer, I want the created Robot objects to be stored in a Fleet and the created Dinosaur objects to be stored in a Herd 
+# (the Fleet and Herd must use a List to store the objects).
 
 #(/10 points): As a developer, I want a Robot to have the ability to attack a Dinosaur and a Dinosaur to have the ability to attack a Robot on a Battlefield.
 
 #(/10 points): As a developer, I want a Robot/Dinosaur to lose health points (loss based on attack power) when another Robot/Dinosaur successfully attacks it.
 
-#(/10 points): As a developer, I want the battle to conclude once either all the robots in the Fleet have their health points reach zero or all of the dinosaurs in the Herd have their health points reach zero.
+#(/10 points): As a developer, I want the battle to conclude once either all the robots in the Fleet have their health points reach zero or 
+# all of the dinosaurs in the Herd have their health points reach zero.
 
 #Bonus points:
 
