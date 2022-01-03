10386b8b37f4247b173dc40ab12e4c231b461851
Lizzy <Lizzy@Lizzys-MacBook-Air.local>
2022-01-03 12:09:00 -0500

    Created classes for each file. Began to fill out the basics of the robot, weapon, and dinosaur classes following the UML

---
+++ robot.py
@@ -0,0 +1,8 @@
+class Robot:
+    def __init__(self, name, weapon):
+        self.name = name
+        self.weapon = weapon
+        self.health = 100 #COME BACK AND ADJUST THIS AS NEEDED
+
+    def attack(self, dino):
+        pass
