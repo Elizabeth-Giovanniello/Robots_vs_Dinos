d7e0ebcccdfc8b0bfa52af6c9878aca8a01ec78d
Lizzy <Lizzy@Lizzys-MacBook-Air.local>
2022-01-04 12:08:27 -0500

    Created a method for choosing from a list of weapon options in the Robot class. Working on creating a method to choose robot competitors from a list of robots in the Fleet class.

--- fleet.py
+++ fleet.py
@@ -1,5 +1,7 @@
 from robot import Robot
 
+import random
+
 class Fleet:
     def __init__(self):
         self.robots = []
@@ -12,8 +14,44 @@ class Fleet:
         self.robots.append(self.robot_two)
         self.robots.append(self.robot_three)
 
-    def dead_robot(self):
-        for robot in self.robots:
-            if robot.health <= 0:
-                self.robots.remove(robot)
-                print(f"Following the last attack, {robot}'s circuitry has taken too much damage, and is no longer functional. {robot} is no longer part of this fleet.")
+
+    def choose_robots(self):
+        print("Before the battle begins, YOU have the opportunity to choose which competitors will represent Team Robot. Your options are: ")
+        self.roomba_weapons = [["shards of glass from a lamp you broke", 5], ["thumb tacks that fell on the floor", 3], ["dust bomb", 10]]
+        self.alexa_weapons = [["a piercingly high-pitched noise that shuts down one's central nervous system", 50], []]
+        self.blender_weapons = [["rotating blades", 20], ["an exploding kale smoothie", 10], ["ice cube projectiles", 25]]
+        self.drone_weapons = [["a chemical bomb", 35], ["a cluster bomb", 40], ["a hand grenade", 20], ["a machine gun", 25], ["a nuclear missile", 90]] 
+        self.r2d2_weapons = [["the Force", 15], ["a lightsaber", 25], ["space laser gun", 20]]
+        self.microwave_weapons = [["a high concentration of electromagnetic waves", 15], ["an irritating beep that won't stop even when its door is opened", 1], ["the sharp corner of its swining door", 5]]
+
+        self.robot_roomba = Robot("Roomba", self.roomba_weapons)
+        self.robot_alexa = Robot("Alexa", self.alexa_weapons)
+        self.robot_blender = Robot("Fruit Ninja Blender", self.blender_weapons)
+        self.robot_drone = Robot("Military-grade Drone", self.drone_weapons)
+        self.robot_r2d2 = Robot("R2D2", self.r2d2_weapons)
+        self.robot_microwave = Robot("Microwave oven", self.microwave_weapons)
+
+        self.robot_competitors = [self.robot_roomba, self.robot_alexa, self.robot_blender, self.robot_drone, self.robot_r2d2, self.robot_microwave]
+
+        self.num = 1
+
+        for item in self.robot_competitors:
+            print(f"{self.num}. {item.name}")
+            self.num += 1
+        self.user_choice = input("Enter the number of the first robot you wish to add to your fleet. Enter any other character for a robot to be randomly assigned. ")
+        if int(self.user_choice) > 0 and int(self.user_choice) <= len(self.robot_competitors):
+            self.robot_index = int(self.user_choice) - 1
+            self.robot_one = self.robot_competitors[self.robot_index]
+        else:
+            self.robot_one = random.choice(self.robot_competitors)
+            print(f"{self.robot_one.name} has been randomly selected as the first member of your fleet. ")
+        
+        self.user_choice = input("Enter the number of the second robot you wish to add to your fleet. Enter any other character for a robot to be randomly assigned. ")
+        if int(self.user_choice) > 0 and int(self.user_choice) <= len(self.robot_competitors):
+            self.robot_index = int(self.user_choice) - 1
+            self.robot_two = self.robot_competitors[self.robot_index]
+        else:
+            self.robot_one = random.choice(self.robot_competitors)
+            print(f"{self.robot_one.name} has been randomly selected as the second member of your fleet. ")
+
+            input("The robot you have selected is already in your fleet. Please select a different competitor. ")
--- robot.py
+++ robot.py
@@ -1,15 +1,44 @@
 from weapon import Weapon
 
+import random
+
 class Robot:
-    def __init__(self, name, weapon_name, weapon_attack):
+    def __init__(self, name, weapon_list):
         self.name = name
-        self.weapon = Weapon(weapon_name, weapon_attack)
-        self.health = 100 #COME BACK AND ADJUST THIS AS NEEDED
+        self.weapon = None
+        self.health = 100
+        self.weapon_options = weapon_list 
 
     def attack(self, dino):
         print(f"{self.name} has attacked {dino.name} with {self.weapon.name}!")
         dino.health -= self.weapon.attack_power
             
+    def choose_weapon(self):
+        print(f"Now that you have chosen {self.name} as one of your robot champions, you must choose its weapon from the following options: ")
+        self.num = 1
+        for item in self.weapon_options:
+            print(f"{self.num}. {item.capitalize()}")
+            self.num += 1
+        self.user_choice = input("Enter the number of the weapon you want your chosen robot champion to bear. Enter any other character for a weapon to be randomly assigned. ")
+        if int(self.user_choice) > 0 and int(self.user_choice) <= len(self.weapon_options):
+            self.weapon_index = int(self.user_choice) - 1
+            self.weapon = Weapon(self.weapon_options[self.weapon_index])
+        else:
+            self.weapon = Weapon(random.choice(self.weapon_options))
+        print(f"{self.name} will be using {self.weapon.name} as its weapon. ")
+
+
+
+
+
+
+
+
+    def __init__(self, name, weapon_name, weapon_attack):
+        self.name = name
+        self.weapon = Weapon(weapon_name, weapon_attack)
+        self.health = 100 
+
 
 
 
